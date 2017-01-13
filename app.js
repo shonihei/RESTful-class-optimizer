5945ae1552593aa6a2829d2139bb43cb00baed6a
shonihei <shonihei@gmail.com>
2017-01-13 16:32:54 +0900

    prototyped web scraping module and implemented it to the actual web application

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var async = require('async');
-var request = require('request');
-var jsdom = require('jsdom');
 
 var index = require('./routes/index');
 var classes = require('./routes/classes');
--- routes/classes.js
+++ routes/classes.js
@@ -6,11 +6,13 @@ var buscraper = require('../node_modules/buscraper');
 
 
 /* GET class info. */
-router.get('/getclasses/:classname', function(req, res) {
-
-    res.json(result);
+router.get('/getclasses/:semester/:classname', function(req, res) {
+    buscraper.getClass(req.params.semester, req.params.classname, function(err, result) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(result);
+    })
 });
-
-
 
 module.exports = router;
