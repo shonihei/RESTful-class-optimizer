e4fdbed0cb6d0abffdad156b0d9db7f34f568357
shonihei <shonihei@gmail.com>
2017-01-17 20:10:31 -0500

    Works on routering logic

--- routes/api.js
+++ routes/api.js
@@ -5,12 +5,25 @@ var jsdom = require('jsdom');
 var buscraper = require('../node_modules/buscraper');
 
 
-/* GET class info. */
-router.get('/getclasses/:semester', function(req, res) {
-    console.log(req.params.classname);
-    buscraper.getClass(req.params.semester, req.params.classname, function(err, result) {
+/* GET classes info. */
+router.get('/getclasses', function(req, res) {
+    let classes = req.query.classname;
+    let semester = req.query.semester;
+    buscraper.getClasses(semester, classes, function(err, result) {
         if(err) {
             res.send(err);
+        }
+        res.json(result);
+    })
+});
+
+/* GET class info. */
+router.get('/getclass', function(req, res) {
+    let className = req.query.classname;
+    let semester = req.query.semester;
+    buscraper.getClass(semester, className, function(err, result) {
+        if(err) {
+            res.status(400).send(err);
         }
         res.json(result);
     })
