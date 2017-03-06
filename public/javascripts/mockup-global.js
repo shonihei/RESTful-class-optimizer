6ff217103410f3319b0e719fc1d46d3ffdbf037e
shonihei <shonihei@gmail.com>
2017-03-06 18:37:12 -0500

    Works on frontend logic/design

--- public/javascripts/mockup-global.js
+++ public/javascripts/mockup-global.js
@@ -1,10 +1,15 @@
 // FOR DEBUGGING PURPOSES
-let semester = "20174"
+requestedData = {};
+currentlyLoading = {};
+let semester = "20174";
 
-// add event listeners
 $(document).ready(function() {
     console.log("Document ready!")
+
+    // Initialize autotab.js
     $('input').autotab();
+
+    // Add submission event listener
     $('.next').on('click', requestData);
 });
 
@@ -17,16 +22,10 @@ function requestData(e) {
     let input = collectUserInput();
 
     // Check input is atleast 1
-    if (input.length == 0) {
-        /*
-        Visual indication and custom error message goes here
-        */
+    if (!input || input.length == 0) {
+        alert("Please enter at least one class");
         return null;
     }
-
-    // Initialize result collections
-    let resultCollection = [];
-    let errorCollection = [];
 
     // Create wrapper method for AJAX
     let ajax_caller = function(data) {
@@ -35,10 +34,20 @@ function requestData(e) {
             method: data.method,
         }).done(function(result) {
             if (result.status == "success") {
-                resultCollection.push(result.data);
+                // Stop loading sign and display success
+                let courseInput = currentlyLoading[result.requestedclass];
+                switchLoading(courseInput, false);
+                switchSuccess(courseInput, true);
+                delete currentlyLoading[result.requestedclass];
+
+                requestedData[result.requestedclass] = result.data;
             }
             else {
-                errorCollection.push(result.requestedclass);
+                // Stop loading sign and display error
+                let courseInput = currentlyLoading[result.requestedclass];
+                switchLoading(courseInput, false);
+                switchWarning(courseInput, true);
+                delete currentlyLoading[result.requestedclass];
             }
         })
     };
@@ -46,6 +55,13 @@ function requestData(e) {
     // Create an array of AJAX calls iteratively
     let ajax_calls = [];
     for (var i = 0; i < input.length; i++) {
+        // Search if the current input has already been fetched
+        if (requestedData[input]) {
+            continue;
+        }
+
+        //console.log("Adding " + input[i] + " to queue")
+
         ajax_calls.push(ajax_caller({
             url: '/api/getclass?semester=' + semester + '&classname=' + input[i],
             method: 'GET',
@@ -54,16 +70,15 @@ function requestData(e) {
 
     // Send requests asynchnously and do something when everything is complete
     $.when.apply(this, ajax_calls).done(function() {
-        // resultCollection = Successful requests
-        // errorCollection = Nonexistent classes were requested
-        console.log("AJAX completed...")
-        console.log(resultCollection);
-        console.log(errorCollection);
+        console.log("AJAX completed...");
     });
 }
 
 // Collect user input from input DOMS
 function collectUserInput() {
+    // Error flag
+    let hasError = false;
+
     // Get each input rows
     let containers = $('.form-container').find('.courseinput');
 
@@ -71,17 +86,36 @@ function collectUserInput() {
     let userInput = [];
 
     // Loop through each row in containers and collect input
+    // in jQuery, return non-falsey value from .each acts as "continue"
     $.each(containers, function(index, value) {
         let fields = $(value).find('input');
+
+        // Initialize parsedString
         let parsedString = ""
+
+        // Create a string
         $.each(fields, function(index, value) {
             parsedString += $(value).val();
         });
 
-        // If the row was left empty, don't validate
-        let leftEmpty = parsedString.length === 0;
+        // Uppercasify
+        parsedString = parsedString.toUpperCase();
 
-        if (!leftEmpty && validInput(parsedString, containers[index])) {
+        // If the row was left empty, don't validate
+        if (parsedString.length === 0) {
+            return true;
+        }
+
+        // If current input has already been fetched, don't do anything
+        if (requestedData[parsedString]) {
+            return true;
+        }
+
+        // Check whether valid
+        let valid = validInput(parsedString, containers[index]);
+
+        if (valid) {
+            currentlyLoading[parsedString.toUpperCase()] = containers[index];
             userInput.push(parsedString.toUpperCase());
         }
     });
@@ -89,22 +123,17 @@ function collectUserInput() {
     return userInput;
 }
 
-let test;
 // Validate the string input by the user
 // Valid input : CASCS111
 // length == 8
 // First 5 must be chars, last 3 must be numbers
-function validInput(str, element) {
-    let loadSign = $(element).find('.feedback')
-    $(loadSign).addClass('showLoad');
+function validInput(str, courseInput) {
 
     // Check that the string is exactly 8 characters long
     if (str.length !== 8) {
-        /*
-        Visual indication and custom error message goes here
-        */
+        switchLoading(courseInput, false);
+        switchWarning(courseInput, true);
 
-        alert(str + " is not a valid class format")
         return false;
     }
 
@@ -114,24 +143,77 @@ function validInput(str, element) {
 
     // if first 5 characters are not letters
     if (!isLetters) {
-        /*
-        Visual indication and custom error message goes here
-        */
-        alert(str + " is not a valid class format")
+        switchLoading(courseInput, false);
+        switchWarning(courseInput, true);
+
         return false;
     }
 
     // if last 3 characters are not numbers
     if (!isNumbers) {
-        /*
-        Visual indication and custom error message goes here
-
-        */
-        alert(str + " is not a valid class format")
+        switchLoading(courseInput, false);
+        switchWarning(courseInput, true);
 
         return false;
     }
 
     // Passed validation!
+    switchLoading(courseInput, true);
+    switchWarning(courseInput, false);
     return true;
+}
+
+function switchLoading(courseInput, state) {
+    // Get feedback container and icon
+    let feedbackContainer = $(courseInput).find('.feedback');
+    let icon = $(feedbackContainer).find('i');
+
+    // If requested state is false, turn the logos off
+    if (state === false) {
+        // Remove loading spinner
+        $(feedbackContainer).removeClass('showLoad');
+        $(icon).removeClass('fa-spinner fa-pulse');
+    }
+
+    // else, turn it on
+    else {
+        $(feedbackContainer).addClass('showLoad');
+        $(icon).addClass('fa-spinner fa-pulse');
+    }
+}
+
+function switchWarning(courseInput, state) {
+    // Get feedback container and icon
+    let feedbackContainer = $(courseInput).find('.feedback');
+    let icon = $(feedbackContainer).find('i');
+
+    // If requested state is false, turn the logos off
+    if (state === false) {
+        $(feedbackContainer).removeClass('hasError');
+        $(icon).removeClass('fa-exclamation-triangle');
+    }
+
+    // else, turn it on
+    else {
+        $(feedbackContainer).addClass('hasError');
+        $(icon).addClass('fa-exclamation-triangle');
+    }
+}
+
+function switchSuccess(courseInput, state) {
+    // Get feedback container and icon
+    let feedbackContainer = $(courseInput).find('.feedback');
+    let icon = $(feedbackContainer).find('i');
+
+    // If requested state is false, turn the logos off
+    if (state === false) {
+        $(feedbackContainer).removeClass('hasSuccess');
+        $(icon).removeClass('fa-check');
+    }
+
+    // else, turn it on
+    else {
+        $(feedbackContainer).addClass('hasSuccess');
+        $(icon).addClass('fa-check');
+    }
 }
